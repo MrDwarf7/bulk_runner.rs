67371c18f1a77e19d3053b7a37600c321eeaef21
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-10-28 13:05:48 +1100

    feat(cli): Formatting & style for cli output + context for user

--- crates/bulk_runner_rs/src/cli.rs
+++ crates/bulk_runner_rs/src/cli.rs
@@ -1,9 +1,7 @@
-// use std::collections::VecDeque;
-// use futures::StreamExt;
-// use dashmap::DashMap as HashMap;
+use std::str::FromStr;
 
 use crate::Result;
-use clap::{command, Parser};
+use clap::{command, Parser, ValueEnum};
 
 use crate::prelude::*;
 
@@ -16,62 +14,87 @@ use crate::prelude::*;
     BulkRunner.rs is a CLI tool designed to facilitate the execution of Blue Prism AutomateC processes in bulk.
     It streamlines the process of launching multiple instances/process via the Control Room, each targeting a distinct resource or bot.
     This is particularly useful during change over periods where multiple bots need to be transitioned from one process to another.",
-    version,
-    arg_required_else_help = true
+    version = std::env!("CARGO_PKG_VERSION"),
+    arg_required_else_help = true,
+    styles=get_styles()
 )]
 pub struct Cli {
     /// The process to run on all the bots pulled by the SQL query.
-    #[arg(
-        //
-        index = 1,
-        help = "The process to run the bots on.",
-        value_hint = clap::ValueHint::Other
-    )]
+    #[arg(index = 1, help = "The process to run the bots on.", value_hint = clap::ValueHint::Other)]
     pub process: String,
 
     /// The number of bots to run concurrently.
     /// This is handled internally via a semaphore, not via the hardware concurrency of the CPU.
-    #[arg(
-        short = 't',
-        long = "total",
-        default_value = "30",
-        //index = 2,
-        long_help = "The number of bots to run concurrently. This is handled internally via a semaphore, not via the hardware concurrency of the CPU.",
-        value_hint = clap::ValueHint::Other
+    #[arg(short = 't', long = "total", default_value = "30", value_hint = clap::ValueHint::Other,
+    long_help = "The number of bots to run concurrently. This is handled internally via a semaphore, not via the hardware concurrency of the CPU.",
     )]
     pub total_bots: usize,
 
     /// Optional path to a SQL file to pull the bots from.
     /// If not provided, the default value is "bots.sql".
     /// And is looked for in the current working directory of the binary.
-    #[arg(
-        short = 'f',
-        long = "file",
-        help = "The path to the SQL file.",
-        default_value = "bots.sql",
-        value_hint = clap::ValueHint::FilePath
+    #[arg(short = 'f', long = "file", help = "The path to the SQL file.", required = false, default_value = "bots.sql", value_hint = clap::ValueHint::FilePath
     )]
     sql_file: Option<PathBuf>,
+
+    /// Optional verbosity level of the logger.
+    /// You may provide this as either a string or a number.
+    ///
+    /// The least verbose as 0 (Error -> Error Only)
+    /// Most verbose as 4 (Trace -> Trace Everything
+    /// If not provided, the default value is "INFO".
+    #[arg(value_enum, name = "verbosity", short = 'l', long = "level", help = "The verbosity level of the logger.", required = false, default_value = "INFO", value_hint = clap::ValueHint::Other)]
+    pub verbosity_level: Option<VerbosityLevel>,
+
+    /// Optional span level of the logger.
+    /// You may provide this as either a string or a number.
+    /// If not provided, the default value is "NONE".
+    ///
+    /// From least to most verbose:
+    /// -> "NONE" (0) - Do not log any spans.
+    /// -> "EXIT" (1) - Only log when exiting a span.
+    /// -> "ENTER" (2) - Only log when entering a span.
+    /// -> "FULL" (3) - Log both entering and exiting a span.
+    #[arg(value_enum, name = "span", short = 's', long = "span", help = "The span level of the logger.", required = false, default_value = "NONE", value_hint = clap::ValueHint::Other)]
+    pub span_type: Option<SpanType>,
+}
+
+/// The verbosity level of the logger.
+///
+/// The least verbose as 0 (Error -> Error Only)
+/// Most verbose as 4 (Trace -> Trace Everything).
+#[derive(Debug, ValueEnum, Clone, Copy, PartialEq, Eq)]
+#[clap(name = "VerbosityLevel", rename_all = "upper")]
+pub enum VerbosityLevel {
+    #[value(name = "ERROR", alias = "error", alias = "Error", alias = "0")]
+    Error,
+    #[value(name = "WARN", alias = "warn", alias = "Warn", alias = "1")]
+    Warn,
+    #[value(name = "INFO", alias = "info", alias = "Info", alias = "2")]
+    Info,
+    #[value(name = "DEBUG", alias = "debug", alias = "Debug", alias = "3")]
+    Debug,
+    #[value(name = "TRACE", alias = "trace", alias = "Trace", alias = "4")]
+    Trace,
+}
+
+#[derive(Debug, ValueEnum, Clone, Copy, PartialEq, Eq)]
+#[clap(name = "SpanType", rename_all = "upper")]
+pub enum SpanType {
+    #[value(name = "NONE", alias = "none", alias = "None", alias = "0")]
+    None,
+    #[value(name = "EXIT", alias = "exit", alias = "Exit", alias = "1")]
+    Exit,
+    #[value(name = "ENTER", alias = "enter", alias = "Enter", alias = "2")]
+    Enter,
+    #[value(name = "FULL", alias = "full", alias = "Full", alias = "3")]
+    Full,
 }
 
 impl Default for Cli {
     fn default() -> Self {
         Self::new()
     }
-}
-
-fn check_automate_exists() -> Result<()> {
-    if std::env::var("BYPASS_AUTOMATEC_CHECK").is_ok() {
-        return Ok(());
-    }
-    let path = std::path::Path::new(&*DEFAULT_EXE_PATH);
-    if !path.exists() {
-        return Err(Error::Generic(format!(
-            "AutomateC does not exist at path: {:?}",
-            path
-        )));
-    }
-    Ok(())
 }
 
 impl Cli {
@@ -85,19 +108,21 @@ impl Cli {
     ///
     /// This is useful for testing purposes.
     pub fn new() -> Self {
-        match check_automate_exists() {
-            Ok(_) => (),
-            Err(e) => {
-                error!("Error: {}", e);
-                std::process::exit(1);
-            }
-        }
+        // match check_automate_exists() {
+        //     Ok(_) => (),
+        //     Err(e) => {
+        //         error!("Tracing Error: {}", e);
+        //         eprintln!("Error: {}", e);
+        //         std::process::exit(1);
+        //     }
+        // }
+        Self::parse()
 
-        let mut s = Self::parse();
-        if s.sql_file.is_none() {
-            s.sql_file = Some(PathBuf::from("bots.sql"));
-        }
-        s
+        // let mut s = Self::parse();
+        // if s.sql_file.is_none() {
+        //     s.sql_file = Some(PathBuf::from("bots.sql"));
+        // }
+        // s
     }
     pub fn process(&self) -> &str {
         &self.process
@@ -110,6 +135,10 @@ impl Cli {
     pub fn sql_file(&self) -> &PathBuf {
         self.sql_file.as_ref().unwrap()
     }
+
+    pub fn verbosity_level(&self) -> VerbosityLevel {
+        self.verbosity_level.unwrap_or(VerbosityLevel::Info)
+    }
 }
 
 impl Cli {
@@ -120,56 +149,136 @@ impl Cli {
         Ok(sql_file_query)
     }
 
-    // pub async fn run(&self) -> Result<()> {
-    //     info!("->> {:<12}", "RUN:: Starting run");
+    pub fn check_automate_exists(self) -> Result<Self> {
+        if std::env::var("BYPASS_AUTOMATEC_CHECK").is_ok() {
+            return Ok(self);
+        }
+        let path = std::path::Path::new(&*DEFAULT_EXE_PATH);
+        if !path.exists() {
+            return Err(Error::AutomateCNotFound);
+        }
+        Ok(self)
+    }
+}
 
-    //     let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
-    //     // Serialize the sql file to a string
-    //     let parsed_sql_file = self.serialize_sql_file()?;
-    //     #[rustfmt::skip]
-    //     info!("->> {:<12} - {:#?}", "RUN:: Parsed SQL file", &parsed_sql_file);
-    //     // Spawn a task to fetch the bots from the database,
-    //     // We can then fill the base_bot with the new data that's come back from the query [name && current_status]
-    //     let query_handle = tokio::spawn(async move {
-    //         let tx = tx.clone();
-    //         info!("->> {:<12}", "RUN::  Querying database...");
-    //         crate::query_controller::query_database(tx, &parsed_sql_file).await;
-    //     });
-    //     // .await?; // NOTE: I don't belive we actually need to await this immediately - We can wait for it whenever as it's via a channel
+impl From<VerbosityLevel> for tracing_subscriber::filter::EnvFilter {
+    fn from(level: VerbosityLevel) -> Self {
+        match level {
+            VerbosityLevel::Error => tracing_subscriber::filter::EnvFilter::new("ERROR"),
+            VerbosityLevel::Warn => tracing_subscriber::filter::EnvFilter::new("WARN"),
+            VerbosityLevel::Info => tracing_subscriber::filter::EnvFilter::new("INFO"),
+            VerbosityLevel::Debug => tracing_subscriber::filter::EnvFilter::new("DEBUG"),
+            VerbosityLevel::Trace => tracing_subscriber::filter::EnvFilter::new("TRACE"),
+        }
+    }
+}
 
-    //     // As the query runs, it will return back a Bot (which will have been filled already, we need the Bot to go to next step)
-    //     let future_bots = tokio::spawn(async move {
-    //         // TODO: STREAM: We can stream here also
-    //         let mut bots = Vec::with_capacity(40);
-    //         while let Some(bot) = rx.recv().await {
-    //             if bot.if_available().is_none() {
-    //                 #[rustfmt::skip]
-    //                 warn!("->> {:<12} - {:?}", "Future Bots:: Bot not available...", &bot);
-    //                 break;
-    //             }
-    //             info!("->> {:<12} - {:?}", "Future Bots:: Bot received...", &bot);
-    //             bots.push(bot.into_future());
-    //         }
-    //         rx.close();
-    //         bots
-    //     })
-    //     .await?;
+impl From<u8> for VerbosityLevel {
+    fn from(level: u8) -> Self {
+        match level {
+            0 => VerbosityLevel::Error,
+            1 => VerbosityLevel::Warn,
+            2 => VerbosityLevel::Info,
+            3 => VerbosityLevel::Debug,
+            4 => VerbosityLevel::Trace,
+            _ => VerbosityLevel::Info,
+        }
+    }
+}
 
-    //     let mut filled_bots: Vec<Bot> = futures::future::join_all(future_bots) // NOTE: This is an into_future -> We can stream this via try_next(), what can we do with this?
-    //         .await
-    //         .into_iter()
-    //         //
-    //         .filter_map(|bot| bot.0)
-    //         .collect();
-    //     info!("->> {:<12} - {:?}", "RUN:: Filled bots", &filled_bots);
+impl FromStr for VerbosityLevel {
+    type Err = Error;
 
-    //     let mut dispatch_bots: VecDeque<(Bot, String)> = VecDeque::with_capacity(filled_bots.len());
-    //     filled_bots.drain(..).for_each(|bot| {
-    //         dispatch_bots.push_back((bot, self.process().to_string()));
-    //     });
+    fn from_str(s: &str) -> Result<Self> {
+        match s.to_uppercase().as_str() {
+            "ERROR" => Ok(VerbosityLevel::Error),
+            "WARN" => Ok(VerbosityLevel::Warn),
+            "INFO" => Ok(VerbosityLevel::Info),
+            "DEBUG" => Ok(VerbosityLevel::Debug),
+            "TRACE" => Ok(VerbosityLevel::Trace),
+            _ => Err(Error::Generic(format!(
+                "Verbosity level: {} is not supported.",
+                s
+            ))),
+        }
+    }
+}
 
-    //     query_handle.await?;
-    //     crate::query_controller::cli_dispatch(dispatch_bots, self.total_bots()).await?;
-    //     Ok(())
-    // }
+impl From<SpanType> for tracing_subscriber::filter::EnvFilter {
+    fn from(level: SpanType) -> Self {
+        match level {
+            SpanType::None => tracing_subscriber::filter::EnvFilter::new("NONE"),
+            SpanType::Exit => tracing_subscriber::filter::EnvFilter::new("EXIT"),
+            SpanType::Enter => tracing_subscriber::filter::EnvFilter::new("ENTER"),
+            SpanType::Full => tracing_subscriber::filter::EnvFilter::new("FULL"),
+        }
+    }
+}
+
+impl From<u8> for SpanType {
+    fn from(level: u8) -> Self {
+        match level {
+            0 => SpanType::None,
+            1 => SpanType::Exit,
+            2 => SpanType::Enter,
+            3 => SpanType::Full,
+            _ => SpanType::None,
+        }
+    }
+}
+
+impl FromStr for SpanType {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        match s.to_uppercase().as_str() {
+            "NONE" => Ok(SpanType::None),
+            "ENTER" => Ok(SpanType::Enter),
+            "EXIT" => Ok(SpanType::Exit),
+            "FULL" => Ok(SpanType::Full),
+            _ => Err(Error::Generic(format!(
+                "Span type: {} is not supported.",
+                s
+            ))),
+        }
+    }
+}
+
+pub fn get_styles() -> clap::builder::Styles {
+    clap::builder::Styles::styled()
+        .usage(
+            anstyle::Style::new()
+                .bold()
+                .underline()
+                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Yellow))), // When a command is inc. This is the tag collor for 'Usage:'
+        )
+        .header(
+            anstyle::Style::new()
+                .bold()
+                .underline()
+                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Blue))), // Main headers in the help menu (e.g. Arguments, Options)
+        )
+        .literal(
+            anstyle::Style::new()
+                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::BrightWhite))), // Strings for args etc { -t, --total }
+        )
+        .invalid(
+            anstyle::Style::new()
+                .bold()
+                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Red))),
+        )
+        .error(
+            anstyle::Style::new()
+                .bold()
+                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Red)))
+                .effects(anstyle::Effects::ITALIC),
+        )
+        .valid(
+            anstyle::Style::new()
+                .bold()
+                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Cyan))),
+        )
+        .placeholder(
+            anstyle::Style::new().fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::White))),
+        )
 }
